9b3d1e6b4275da456552cf0a2860d1472b474e21
SubramaniyamJ <117516466+SubramaniyamJ@users.noreply.github.com>
2024-02-29 09:36:42 +0530

    deleted

--- backend/springapp-hasc/src/main/java/com/example/springapphasc/repository/UserRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.springapphasc.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.springapphasc.model.User;
-
-public interface UserRepo extends JpaRepository<User, Integer>{
-    
-}
